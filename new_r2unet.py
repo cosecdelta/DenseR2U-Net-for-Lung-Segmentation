2691cb7a754e6b84ddf9c4b93f9606037d740b2a
Kaushik <63477668+cosecdelta@users.noreply.github.com>
2021-06-09 15:56:22 -0500

    latest fix

---
+++ new_r2unet.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 00:35:36 2021
+
+@author: kaushik.dutta
+"""
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Input, Dropout, Add, Activation, UpSampling2D,  Concatenate
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras import backend as K
+import numpy as np
+from tensorflow.keras.metrics import Precision, Recall, AUC, Accuracy
+
+
+K.set_image_data_format('channels_last')
+
+smooth = 1
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (1 -(2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+
+"""Recurrent Layer"""
+def rec_layer(layer, filters):
+    reconv1 = Conv2D(filters, (3, 3), activation='relu', padding='same')(layer)
+    #drop_inter = Dropout(0.3)(reconc1)
+    reconv1 = Conv2D(filters, (3, 3), activation='relu', padding='same')(reconv1)
+    return reconv1
+
+########## Initialization of Parameters #######################
+image_row = 128
+image_col = 128
+image_depth = 2
+
+def r2unet():
+    inputs = inputs = Input((image_row, image_col, image_depth))
+    conv1 = rec_layer(inputs,32)
+    conv1 = rec_layer(conv1,32)
+    conv1add = Conv2D(32, kernel_size=(1, 1), padding='same')(inputs)
+    add1 = Add()([conv1add, conv1])
+    #dense1 = concatenate([add1, conv1], axis=3)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(add1)
+
+    conv2 = rec_layer(pool1, 64)
+    conv2 = rec_layer(conv2, 64)
+    conv2add = Conv2D(64, kernel_size=(1, 1), padding='same')(pool1)
+    add2 = Add()([conv2add, conv2])
+    #dense2 = concatenate([add2, conv2], axis=3)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(add2)
+
+    conv3 = rec_layer(pool2, 128)
+    conv3 = rec_layer(conv3, 128)
+    conv3add = Conv2D(128, kernel_size=(1, 1), padding='same')(pool2)
+    add3 = Add()([conv3add, conv3])
+    #dense3 = concatenate([add3, conv3], axis=3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(add3)
+
+    conv4 = rec_layer(pool3, 256)
+    conv4 = rec_layer(conv4, 256)
+    conv4add = Conv2D(256, kernel_size=(1, 1), padding='same')(pool3)
+    add4 = Add()([conv4add, conv4])
+    #dense4 = concatenate([add4, conv4], axis=3)
+    #drop4 = Dropout(0.4)(dense4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(add4)
+
+    conv5 = rec_layer(pool4, 512)
+    conv5 = rec_layer(conv5, 512)
+    conv5add = Conv2D(512, kernel_size=(1, 1), padding='same')(pool4)
+    add5 = Add()([conv5add, conv5])
+    #dense5 = concatenate([add5, conv5], axis=3)
+    drop5 = Dropout(0.4)(add5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(drop5), conv4], axis=3)
+    conv6 = rec_layer(up6, 256)
+    conv6 = rec_layer(conv6, 256)
+    conv6add = Conv2D(256, kernel_size=(1, 1), padding='same')(up6)
+    add6 = Add()([conv6add, conv6])
+    #dense6 = concatenate([add6, conv6], axis=3)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(add6), conv3], axis=3)
+    conv7 = rec_layer(up7, 128)
+    conv7 = rec_layer(conv7, 128)
+    conv7add = Conv2D(128, kernel_size=(1, 1), padding='same')(up7)
+    add7 = Add()([conv7add, conv7])
+    #dense7 = concatenate([add7, conv7], axis=3)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(add7), conv2], axis=3)
+    conv8 = rec_layer(up8, 64)
+    conv8 = rec_layer(conv8, 64)
+    conv8add = Conv2D(64, kernel_size=(1, 1), padding='same')(up8)
+    add8 = Add()([conv8add, conv8])
+    #dense8 = concatenate([add8, conv8], axis=3)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(add8), conv1], axis=3)
+    conv9 = rec_layer(up9, 64)
+    conv9 = rec_layer(conv9, 64)
+    conv9add = Conv2D(64, kernel_size=(1, 1), padding='same')(up9)
+    add9 = Add()([conv9add, conv9])
+    #dense9 = concatenate([add9, conv9], axis=3)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(add9)
+    model = Model(inputs=[inputs], outputs=[conv10])
+    model.summary()
+    model.compile(optimizer=Adam(lr=1e-5), loss= dice_loss, metrics=[dice_coef, Precision(), Recall(), AUC(), Accuracy()])
+    pretrained_weights = None
+
+    if(pretrained_weights):
+        model.load_weights(pretrained_weights)
+        
+    return model
+
--- res_unet.py
+++ res_unet.py
@@ -8,7 +8,7 @@ Created on Wed Jun 24 11:25:07 2020
 
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Input, Dropout
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, Input, Dropout, Add
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras import backend as K
@@ -31,55 +31,64 @@ def dice_loss(y_true, y_pred):
     
 
 ########## Initialization of Parameters #######################
-image_row = 256
-image_col = 256
-image_depth = 1
+image_row = 128
+image_col = 128
+image_depth = 2
 
 def resunet():
     inputs = Input((image_row, image_col, image_depth))
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-    conc1 = concatenate([inputs, conv1], axis=3)
+    input_new = Conv2D(32, (1, 1), activation='relu', padding='same')(inputs)
+    conc1 = Add()([input_new, conv1])
     pool1 = MaxPooling2D(pool_size=(2, 2))(conc1)
 
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    conc2 = concatenate([pool1, conv2], axis=3)
+    input_new = Conv2D(64, (1, 1), activation='relu', padding='same')(pool1)
+    conc2 = Add()([input_new, conv2])
     pool2 = MaxPooling2D(pool_size=(2, 2))(conc2)
 
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-    conc3 = concatenate([pool2, conv3], axis=3)
+    input_new = Conv2D(128, (1, 1), activation='relu', padding='same')(pool2)
+    conc3 = Add()([input_new, conv3])
     pool3 = MaxPooling2D(pool_size=(2, 2))(conc3)
 
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
-    conc4 = concatenate([pool3, conv4], axis=3)
+    input_new = Conv2D(256, (1, 1), activation='relu', padding='same')(pool3)
+    conc4 = Add()([input_new, conv4])
     pool4 = MaxPooling2D(pool_size=(2, 2))(conc4)
 
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
-    conc5 = concatenate([pool4, conv5], axis=3)
+    input_new = Conv2D(512, (1, 1), activation='relu', padding='same')(pool4)
+    conc5 = Add()([input_new, conv5])
 
     up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conc5), conv4], axis=3)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
     conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
-    conc6 = concatenate([up6, conv6], axis=3)
+    input_new = Conv2D(256, (1, 1), activation='relu', padding='same')(up6)
+    conc6 = Add()([input_new, conv6])
 
     up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conc6), conv3], axis=3)
     conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
     conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
-    conc7 = concatenate([up7, conv7], axis=3)
+    input_new = Conv2D(128, (1, 1), activation='relu', padding='same')(up7)
+    conc7 = Add()([input_new, conv7])
 
     up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conc7), conv2], axis=3)
     conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
     conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
-    conc8 = concatenate([up8, conv8], axis=3)
+    input_new = Conv2D(64, (1, 1), activation='relu', padding='same')(up8)
+    conc8 = Add()([input_new, conv8])
 
     up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conc8), conv1], axis=3)
     conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
     conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
-    conc9 = concatenate([up9, conv9], axis=3)
+    input_new = Conv2D(32, (1, 1), activation='relu', padding='same')(up9)
+    conc9 = Add()([input_new, conv9])
 
     conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conc9)
 
@@ -88,7 +97,7 @@ def resunet():
     model.summary()
     #plot_model(model, to_file='model.png')
 
-    model.compile(optimizer=Adam(lr=1e-5), loss= 'binary_crossentropy', metrics=[dice_coef])
+    model.compile(optimizer=Adam(lr=1e-5), loss= dice_loss, metrics=[dice_coef])
     
     pretrained_weights = None
 
